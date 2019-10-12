542666e885b140607395e77cfe9f0fa8243ac66c
pravin <pkanwate1234@gmail.com>
2019-10-12 10:55:01 +0530

    second

---
+++ src/main/java/com/example/devnew/DevnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.devnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnewApplication.class, args);
+	}
+
+}
